1bd5c12587036208fd54b454e9a98467f1893091
landin_lin <landin.lin@tinklabs.com>
2019-02-25 21:51:45 +0800

    add a uint test method

--- src/test/java/com/tinklabs/LocationServiceApplicationTests.java
+++ src/test/java/com/tinklabs/LocationServiceApplicationTests.java
@@ -1,16 +1,23 @@
 package com.tinklabs;
 
+import com.tinklabs.mapper.CurrencyMapper;
+import com.tinklabs.vo.CurrencyVo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LocationServiceApplicationTests {
-
+    @Resource
+    private CurrencyMapper currencyMapper;
     @Test
     public void contextLoads() {
+        List<CurrencyVo> currencyList = currencyMapper.queryCurrencyList("CN");
     }
 
 }
